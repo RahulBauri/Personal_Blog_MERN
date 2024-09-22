1f428934bd887372bf5f1bf1b8242a8f968ce244
rahul bauri <kumarrahulbauri2000@gmail.com>
2024-09-22 20:19:17 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,37 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Home, About, SignIn, SignUp, Dashboard, Projects } from './pages';
+import Layout from './components/Layout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/sign-in',
-    element: <SignIn />,
-  },
-  {
-    path: '/sign-up',
-    element: <SignUp />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-  {
-    path: '/projects',
-    element: <Projects />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: 'sign-up',
+        element: <SignUp />,
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: 'projects',
+        element: <Projects />,
+      },
+    ],
   },
 ]);
 
