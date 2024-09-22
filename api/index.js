12ab0ff260a1a5915ffe3ad11d18484f6f835a25
rahul bauri <kumarrahulbauri2000@gmail.com>
2024-09-22 22:26:45 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    return console.log('MongoDB is connected!');
+  })
+  .catch((err) => {
+    return console.log(err);
+  });
 
 const app = express();
 const PORT = 3000;
@@ -10,3 +23,5 @@ app.get('/test', (req, res) => {
 app.listen(PORT, () => {
   console.log(`server is listening on the port ${PORT}...`);
 });
+
+// mongodb+srv://Rahul:I1jJGfsfaFRluvT7@nodeexpressprojects.bjn2qat.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects/Personal_Blog_App
